9557a2d4aeb6f19ca27eec6e2752dae6683c72a3
abhishekaccenture <abhishek.a.dasgupta@accenture.com>
2017-07-31 16:22:43 +0530

    Update EventServerApp.scala

--- src/main/scala/EventServerApp.scala
+++ src/main/scala/EventServerApp.scala
@@ -2,7 +2,7 @@ import io.prediction.data.api.{EventServer, EventServerConfig}
 
 object EventServerApp extends App {
 
-  val port = sys.env.getOrElse("PORT", "7070").toInt
+  val port = sys.env.getOrElse("PORT", "5432").toInt
 
   EventServer.createEventServer {
     EventServerConfig(ip = "0.0.0.0", port = port)
