94a35a848bcc57ed37fa4a23c8cd555d96b6d6a0
abhishekaccenture <abhishek.a.dasgupta@accenture.com>
2017-07-31 16:41:21 +0530

    Update EventServerApp.scala

--- src/main/scala/EventServerApp.scala
+++ src/main/scala/EventServerApp.scala
@@ -5,7 +5,7 @@ object EventServerApp extends App {
   val port = sys.env.getOrElse("PORT", "5432").toInt
 
   EventServer.createEventServer {
-    EventServerConfig(ip = "0.0.0.0", port = port)
+    EventServerConfig(ip = "https://adg-pio-eventserver.herokuapp.com", port = port)
   }
 
 }
